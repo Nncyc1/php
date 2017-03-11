df505ce4a429ab09cab4dd86d17e77878b4b2e57
testica <leitotesta@gmail.com>
2017-03-10 23:28:26 -0400

    second commit

--- encuesta.php
+++ encuesta.php
@@ -1,32 +1,24 @@
 <?php
 
 
- $host = 'localhost'; 
- $user = 'root'; 
- $pass = ''; 
- $db = 'test2'; 
- 
- $con = mysqli_connect($host,$user,$pass,$db); 
- 
- if(!$con) {
-     
-     die("Error ".mysqli_connect_error()); 
-     
- } 
- 
- $sql = "select * from seccion where email='$email'"; 
- $res = mysqli_query($con,$sql); 
- $response = array(); 
- 
+include_once 'connection.php';
+
+
+ $db = new DB_Connection();
+ $con = $db->getConnection();
+
+ $sql = "select * from seccion where email='$email'";
+ $res = mysqli_query($con,$sql);
+ $response = array();
+
  while($row=mysqli_fetch_array($res)) {
-     
+
     array_push($response,array('id'=>$row['id']));
-        
- } 
- 
- echo json_encode(array('result'=>$response)); 
- 
-      mysqli_close($con);  
+
+ }
+
+ echo json_encode(array('result'=>$response));
+
+      mysqli_close($con);
 
 ?>﻿
-    
--- respaldodeseccion.php
+++ respaldodeseccion.php
@@ -1,31 +1,20 @@
 <?php
 
- $host = 'localhost'; 
- $user = 'root'; 
- $pass = ''; 
- $db = 'test2'; 
- 
- $con = mysqli_connect($host,$user,$pass,$db); 
- 
- if(!$con) {
-     
-     die("Error ".mysqli_connect_error()); 
-     
- } 
- 
- $sql = "select * from seccion"; 
- $res = mysqli_query($con,$sql); 
- $response = array(); 
- 
+include_once 'connection.php';
+$db = new DB_Connection();
+$con = $db->getConnection();
+ $sql = "select * from seccion";
+ $res = mysqli_query($con,$sql);
+ $response = array();
+
  while($row=mysqli_fetch_array($res)) {
-     
+
     array_push($response,array('pregunta'=>$row['pregunta']));
-        
- } 
- 
- echo json_encode(array('result'=>$response)); 
- 
-      mysqli_close($con);  
+
+ }
+
+ echo json_encode(array('result'=>$response));
+
+      mysqli_close($con);
 
 ?>﻿
-    
